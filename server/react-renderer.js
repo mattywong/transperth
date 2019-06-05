6995ffd1836b9979b47dd6d7e0b0bdfbd2f32581
mattw <mattywong94@gmail.com>
2019-06-06 00:22:13 +0800

    stream contents

--- server/react-renderer.js
+++ server/react-renderer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import express from "express";
 import path from "path";
-import { renderToString } from "react-dom/server";
+import { renderToString, renderToNodeStream } from "react-dom/server";
 import { ServerStyleSheet } from "styled-components";
 
 import fs from "fs";
@@ -11,25 +11,34 @@ const template = fs.readFileSync(_path, "utf8");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
+  res.write("<!DOCTYPE html><html><head><title>My Page</title></head><body>");
+  res.write("<div id='root'>");
   await import("../client/src/App.js")
     .then(Component => {
       const sheet = new ServerStyleSheet();
-      let html, styleTags;
-      try {
-        html = renderToString(sheet.collectStyles(<Component.default />));
-        styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
 
-        console.log(styleTags);
-      } catch (error) {
-        // handle error
-        console.error(error);
-      } finally {
-        sheet.seal();
-      }
+      const jsx = sheet.collectStyles(<Component.default />);
 
-      let page = template.replace("<!-- CONTENT -->", html);
-      page = page.replace("<!-- STYLES -->", styleTags);
-      res.send(page);
+      const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx));
+
+      stream.pipe(
+        res,
+        { end: false }
+      );
+
+      stream.on("end", () => res.end("</body></html>"));
+
+      //   console.log(styleTags);
+      // } catch (error) {
+      //   // handle error
+      //   console.error(error);
+      // } finally {
+      //   sheet.seal();
+      // }
+
+      // let page = template.replace("<!-- CONTENT -->", html);
+      // page = page.replace("<!-- STYLES -->", styleTags);
+      // res.send(page);
     })
     .catch(next);
 });
