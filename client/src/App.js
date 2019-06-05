86c462f701164b78164fe3f9bc9456b2212e8b6b
mattw <mattywong94@gmail.com>
2019-06-06 00:56:35 +0800

    server rendering react router + global style sheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+
+import { Route, Link } from "react-router-dom";
 import HelloWorld from "./ui/HelloWorld";
 import { hot } from "react-hot-loader/root";
+import { createGlobalStyle } from "styled-components";
 
-const App = props => <HelloWorld />;
+const App = props => {
+  const GlobalStyle = createGlobalStyle`
+    body {
+        background: beige;
+        margin: 0;
+    }
+`;
+
+  return (
+    <>
+      <GlobalStyle />
+      <Link to="/">Home</Link>
+      <Link to="/users">Users</Link>
+      <Link to="/about">About</Link>
+
+      <Route path="/" exact render={() => <p>Home</p>} />
+      <Route path="/about/" render={() => <p>About</p>} />
+      <Route path="/users/" component={HelloWorld} />
+    </>
+  );
+};
 
 export default hot(App);
--- server/react-renderer.js
+++ server/react-renderer.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import { renderToNodeStream } from "react-dom/server";
 import { ServerStyleSheet } from "styled-components";
+import { StaticRouter } from "react-router-dom";
 
 // import fs from "fs";
 
@@ -10,14 +11,18 @@ const _path = path.resolve(__dirname, "../client/index.html");
 // const template = fs.readFileSync(_path, "utf8");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("*", async (req, res, next) => {
   res.write("<!DOCTYPE html><html><head><title>My Page</title></head><body>");
   res.write("<div id='root'>");
   await import("../client/src/App.js")
     .then(Component => {
       const sheet = new ServerStyleSheet();
-
-      const jsx = sheet.collectStyles(<Component.default />);
+      console.log(req);
+      const jsx = sheet.collectStyles(
+        <StaticRouter location={req.originalUrl} context={{}}>
+          <Component.default />
+        </StaticRouter>
+      );
 
       const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx));
 
